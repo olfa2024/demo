cba1c5b578230ba65627aa9c6cf03a454cbb93db
najehsalem <najehsalem2023@gmail.com>
2024-02-16 12:57:36 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { RegisterComponent } from './register/register.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NidamComponent } from './nidam/nidam.component';
+import { MawakitComponent } from './mawakit/mawakit.component';
+import { WasfaComponent } from './wasfa/wasfa.component';
+import { DoaaComponent } from './doaa/doaa.component';
+
+const routes: Routes = [
+     {path: '', component:  AccueilComponent ,pathMatch:'full'},
+  {path: 'accueil', component:AccueilComponent},
+  {path: 'inscription', component:InscriptionComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'nidam', component:NidamComponent},
+  {path: 'mawakit', component:MawakitComponent},
+   {path: 'wasfa', component:WasfaComponent},
+   {path: 'doaa', component: DoaaComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
